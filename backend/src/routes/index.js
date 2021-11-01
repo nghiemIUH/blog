481e149212a6f2c37dc00d7103e0deeb56db8cb9
nghiemIUH <vannghiem848@gmail.com>
2021-11-01 11:23:49 +0700

    update backend nodejs

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,8 @@ const bookRouter = require("./Book");
 const upload = require("../config/upload/upload");
 
 route = (app) => {
-    app.use("/user", upload.uploadAvatar.single("img"), userRouter);
-    app.use("/book", upload.uploadBook.single("img"), bookRouter);
+    app.use("/user", upload.uploadAvatar.single("avatar"), userRouter);
+    app.use("/book", upload.uploadBook.single("image"), bookRouter);
 };
 
 module.exports = route;
